c1730ce2081a262bab5a4695ce052c81a1008969
wahidrana2108 <wahidzamanrana@gmail.com>
2023-05-22 00:27:00 +0600

    databse added

---
+++ database/ambulance.sql
@@ -0,0 +1,368 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2023 at 08:25 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ambulance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(10) NOT NULL,
+  `admin_name` varchar(255) NOT NULL,
+  `admin_email` varchar(255) NOT NULL,
+  `admin_pass` varchar(255) NOT NULL,
+  `admin_image` text NOT NULL,
+  `admin_country` text NOT NULL,
+  `admin_about` text NOT NULL,
+  `admin_contact` varchar(255) NOT NULL,
+  `admin_job` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `admin_name`, `admin_email`, `admin_pass`, `admin_image`, `admin_country`, `admin_about`, `admin_contact`, `admin_job`) VALUES
+(1, 'Maisha Khatun', 'admin@gmail.com', '$2y$10$xJiURcPVNTGcq86OSDYo5.6TXtddnDM9FAXhMzmlidXyeX0hvJS26', 'admin.png', 'Bangladesh', 'Nothing', '01770888280', 'Entry');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ambulance`
+--
+
+CREATE TABLE `ambulance` (
+  `con_id` int(10) NOT NULL,
+  `con_name` varchar(255) NOT NULL,
+  `con_ride` int(10) NOT NULL,
+  `con_point` int(11) NOT NULL,
+  `con_img` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `booking` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ambulance`
+--
+
+INSERT INTO `ambulance` (`con_id`, `con_name`, `con_ride`, `con_point`, `con_img`, `date`, `booking`) VALUES
+(23, 'Bouer Doa', 96, 1, 'bd.png', '2023-05-21 17:35:27', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `blog_id` int(10) NOT NULL,
+  `blog_title` varchar(255) NOT NULL,
+  `blog_desc` longtext NOT NULL,
+  `blog_img` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(10) NOT NULL,
+  `cat_title` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_title`, `date`) VALUES
+(8, 'AC', '2023-05-19 17:48:21'),
+(9, 'Non AC', '2023-05-19 17:48:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(10) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `customer_email` varchar(255) NOT NULL,
+  `customer_pass` varchar(255) NOT NULL,
+  `customer_country` text NOT NULL,
+  `customer_contact` varchar(255) NOT NULL,
+  `customer_image` text NOT NULL,
+  `sub_id` int(1) NOT NULL DEFAULT 1,
+  `token` varchar(20) NOT NULL,
+  `confirmation` int(1) NOT NULL DEFAULT 0,
+  `customer_ip` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `customer_name`, `customer_email`, `customer_pass`, `customer_country`, `customer_contact`, `customer_image`, `sub_id`, `token`, `confirmation`, `customer_ip`) VALUES
+(7, 'Wahid Rana', 'wahidrana00000@gmail.com', '$2y$10$xJiURcPVNTGcq86OSDYo5.6TXtddnDM9FAXhMzmlidXyeX0hvJS26', 'Bosnia & Herzegovina', '123456', '332908387_863365958086817_5245945967472384987_n.jpg', 2, '', 1, '::1'),
+(9, 'Wahid Rana', 'wahidzamanrana@gmail.com', '$2y$10$oNhaiVc7eNI947izKoA4S.Bi3R6kKxQzAQFPglNabnTnaR/194Q6S', 'Bosnia & Herzegovina', '123456', '334757836_950522859419785_174301238863540932_n.jpg', 2, '76873f74fc', 1, '::1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `developers`
+--
+
+CREATE TABLE `developers` (
+  `dev_id` int(10) NOT NULL,
+  `dev_name` varchar(255) NOT NULL,
+  `dev_img` text NOT NULL,
+  `dev_cat` varchar(255) NOT NULL,
+  `dev_bio` longtext NOT NULL,
+  `dev_cat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `developers`
+--
+
+INSERT INTO `developers` (`dev_id`, `dev_name`, `dev_img`, `dev_cat`, `dev_bio`, `dev_cat_id`) VALUES
+(1, 'Nishat Tasnim Niloy', 'supervisor.jpg', 'Supervisor', 'CONTINUOUS DEVELOPMENT is better than DELAYED PERFECTION.', 1),
+(2, 'Md. Wahiduzzaman', 'wahid.jpg', '2019-2-60-048', 'I am passionate Full Stack Web Developer from Bangladesh\r\n', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver`
+--
+
+CREATE TABLE `driver` (
+  `driver_id` int(10) NOT NULL,
+  `con_id` int(10) NOT NULL,
+  `cat_id` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `driver_name` varchar(255) NOT NULL,
+  `driver_age` int(10) NOT NULL,
+  `driver_height` int(10) NOT NULL,
+  `driver_weight` int(10) NOT NULL,
+  `driver_rating` int(10) NOT NULL,
+  `driver_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `driver`
+--
+
+INSERT INTO `driver` (`driver_id`, `con_id`, `cat_id`, `date`, `driver_name`, `driver_age`, `driver_height`, `driver_weight`, `driver_rating`, `driver_img`) VALUES
+(32, 23, 8, '2023-05-19', 'Maisha Khatun', 69, 170, 70, 900, 'maisha.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `news_id` int(10) NOT NULL,
+  `news_title` varchar(255) NOT NULL,
+  `news_desc` longtext NOT NULL,
+  `news_img` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE `slider` (
+  `slide_id` int(10) NOT NULL,
+  `slide_name` varchar(255) NOT NULL,
+  `slide_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`slide_id`, `slide_name`, `slide_image`) VALUES
+(1, 'slider-1', 'slider-1.png'),
+(2, 'slider-3', 'slider-3.png'),
+(3, 'slider-2', 'slider-2.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription`
+--
+
+CREATE TABLE `subscription` (
+  `sub_id` int(10) NOT NULL,
+  `sub_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subscription`
+--
+
+INSERT INTO `subscription` (`sub_id`, `sub_title`) VALUES
+(1, 'General User'),
+(2, 'Premium User');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `ambulance`
+--
+ALTER TABLE `ambulance`
+  ADD PRIMARY KEY (`con_id`);
+
+--
+-- Indexes for table `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`blog_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `developers`
+--
+ALTER TABLE `developers`
+  ADD PRIMARY KEY (`dev_id`);
+
+--
+-- Indexes for table `driver`
+--
+ALTER TABLE `driver`
+  ADD PRIMARY KEY (`driver_id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`news_id`);
+
+--
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`slide_id`);
+
+--
+-- Indexes for table `subscription`
+--
+ALTER TABLE `subscription`
+  ADD PRIMARY KEY (`sub_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ambulance`
+--
+ALTER TABLE `ambulance`
+  MODIFY `con_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `blog_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `developers`
+--
+ALTER TABLE `developers`
+  MODIFY `dev_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `driver`
+--
+ALTER TABLE `driver`
+  MODIFY `driver_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `news_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `slide_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `subscription`
+--
+ALTER TABLE `subscription`
+  MODIFY `sub_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
